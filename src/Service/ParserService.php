b03fbe849d904432c180cca5116359d95ed7c1a6
Sacha COHEN <contact@sacha-cohen.fr>
2021-07-01 12:38:43 +0200

    Initial commit

---
+++ src/Service/ParserService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Service;
+
+use DOMXPath;
+use DOMDocument;
+
+class ParserService {
+    /**
+     * @param string $request the XML string response
+     * 
+     * @return array
+     */
+    public function parse(string $request): array {
+        $document = new DOMDocument();
+        $document->loadXML($request);
+        $xpath = new DOMXPath($document);
+
+        $items = [];
+        foreach ($xpath->evaluate('//ns1:getStationDetailsResponse') as $tableNode) {
+            $items[] = [
+                trim(
+                    $xpath->evaluate('string(ns1:comLieu)', $tableNode),
+                ),
+                trim(
+                    $xpath->evaluate('string(ns1:passage/ns1:destination)', $tableNode)
+                )
+            ];
+        }
+
+        foreach ($xpath->evaluate('//ns1:passage') as $tableNode) {
+            $next = $xpath->evaluate('string(ns1:heurePassageReel)', $tableNode);
+            $diff = date_diff(date_create($next), date_create());
+            $diffMinute = $diff->format('%i');
+
+            if ($diffMinute == 0) {
+                $diffText = $diff->format('%s secondes');
+            } else {
+                $diffText = $diff->format('%i minutes %s');
+            }
+
+            $items[] = [
+                trim(
+                    $next
+                ),
+                $diffText,
+                'heeey'
+            ];
+        }
+
+        return $items;
+    }
+}
